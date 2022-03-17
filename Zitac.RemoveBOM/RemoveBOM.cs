44a7902a8808b8dc786652e3e27c9f755496f930
Vigersand <kenny.vigersand@zitac.se>
2022-03-17 09:34:12 +0100

    Init

---
+++ Zitac.RemoveBOM/RemoveBOM.cs
@@ -0,0 +1,31 @@
+using DecisionsFramework.Design.Flow;
+using System.IO;
+using System.Text;
+using System.Linq;
+
+namespace Zitac.RemoveBOM
+{
+
+    [AutoRegisterMethodsOnClass(true, "File Management", "BOM")]
+    public class RemoveBOMSteps {
+
+        public byte[] RemoveBOM(byte[] File)
+        {
+            using (StreamReader sr = new StreamReader(new MemoryStream(File), Encoding.UTF8))
+            {
+                var bytesWithoutBOM = new UTF8Encoding(false).GetBytes(sr.ReadToEnd());
+                return bytesWithoutBOM;
+            }
+        }
+
+        public bool IsBOM(byte[] File)
+        {
+            var enc = new UTF8Encoding(true);
+            var preamble = enc.GetPreamble();
+            if (preamble.Where((p, i) => p != File[i]).Any()) 
+                return false;
+            else
+                return true;
+        }
+    }
+}
